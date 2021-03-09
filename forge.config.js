c4552d1d51f187d301a7f572181bdd4660080486
Zachinquarantine <69423184+Zachinquarantine@users.noreply.github.com>
2021-03-08 21:22:50 -0500

    Add files via upload

---
+++ forge.config.js
@@ -0,0 +1,15 @@
+{
+    name: '@electron-forge/maker-squirrel'
+    config: {
+    }
+  }
+  {
+    name: '@electron-forge/publisher-github',
+    config; {
+      repository: {
+        owner: 'Zachinquarantine',
+        name; 'stbe2021launcherinstaller'
+      }
+      prerelease: true
+    }
+  }
